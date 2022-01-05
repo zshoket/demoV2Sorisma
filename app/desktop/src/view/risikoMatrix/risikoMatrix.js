f979517b9990bca7f341e6b3416749f4799c93e0
Zohaib Shoket <zohaib.shoket@myview.de>
2022-01-05 09:26:09 +0100

    Updated Work for RiskProfile

--- app/desktop/src/view/risikoMatrix/risikoMatrix.js
+++ app/desktop/src/view/risikoMatrix/risikoMatrix.js
@@ -19,7 +19,7 @@ Ext.define("SORISMA.view.risikoMatrix.risikoMatrix", {
   defaultType: "panel",
 
   // html: '<iframe src="html/graph.html" scrolling="no" height="900" width="100%"></iframe>',
-  html: '<iframe src="html/bewurtungMatrix.html" scrolling="no" height="900" width="100%"></iframe>',
+  html: '<iframe src="html/bewurtungMatrix.html" scrolling="yes" height="900" width="100%"></iframe>',
   items: [
     // {
     //   xtype: "button",
@@ -31,17 +31,17 @@ Ext.define("SORISMA.view.risikoMatrix.risikoMatrix", {
     //   tooltip: "Informationen über diese Seite und was Sie hier tun können",
     //   handler: "showInfo",
     // },
-    {
-      xtype: "button",
-      text: "Weiter",
-      iconCls: "x-fa fa-3x fa-forward",
-      docked: true,
-      width: "8%",
-      floating: true,
-      style: { position: "absolute", left: "91%", top: "96%" },
-      docked: "bottom",
-      tooltip: "Weiter zu Visualisierung",
-      handler: "onButtonForward",
-    },
+    // {
+    //   xtype: "button",
+    //   text: "Weiter",
+    //   iconCls: "x-fa fa-3x fa-forward",
+    //   docked: true,
+    //   width: "8%",
+    //   floating: true,
+    //   style: { position: "absolute", left: "91%", top: "96%" },
+    //   docked: "bottom",
+    //   tooltip: "Weiter zu Visualisierung",
+    //   handler: "onButtonForward",
+    // },
   ],
 });
