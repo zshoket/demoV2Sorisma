1d2bad771336a4a3f175bc62af77277c61db77d4
Zohaib Shoket <zohaib.shoket@myview.de>
2022-04-14 14:16:25 +0200

    Update with LogicTest3

--- app/desktop/src/view/bewurtungTest/bewurtungTest.js
+++ app/desktop/src/view/bewurtungTest/bewurtungTest.js
@@ -1,26 +1,25 @@
 Ext.define("SORISMA.view.bewurtungTest.bewurtungTest", {
-  extend: "Ext.panel.Panel",
-  xtype: "bewurtungTest",
-  controller: "varZweiController",
-  viewModel: {
-    type: "varZweiModel",
-  },
-  requires: [
-    "SORISMA.view.bewurtungTest.bewurtungTestController",
-    "SORISMA.view.bewurtungTest.bewurtungTestModel",
-    "Ext.form.FieldSet",
-    "Ext.form.field.*",
-    "Ext.navigation.View",
-    "Ext.container.*",
-  ],
-  scrollable: true,
+    extend: "Ext.panel.Panel",
+    xtype: "bewurtungTest",
+    controller: "varZweiController",
+    viewModel: {
+        type: "varZweiModel",
+    },
+    requires: [
+        "SORISMA.view.bewurtungTest.bewurtungTestController",
+        "SORISMA.view.bewurtungTest.bewurtungTestModel",
+        "Ext.form.FieldSet",
+        "Ext.form.field.*",
+        "Ext.navigation.View",
+        "Ext.container.*",
+    ],
+    scrollable: true,
 
-  defaultType: "panel",
+    defaultType: "panel",
 
-  html: [
-    '<iframe src="html/autoBewurtung.html" scrolling="no" height="900px" width="100%" ></iframe>',
-  ],
+    html: [
+        '<iframe src="html/autoBewurtung.html" scrolling="yes" height="900px" width="100%" ></iframe>',
+    ],
 
-  items: [
-  ],
+    items: [],
 });
